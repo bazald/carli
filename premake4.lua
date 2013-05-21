0a82ab91398ff0bcf789d0f49de06ff4b24f70ee
Mitchell Keith Bloch <bazald@gmail.com>
2013-05-21 02:12:45 -0400

    Fix Code::Blocks git.h generation for the last time?

--- premake4.lua
+++ premake4.lua
@@ -39,42 +39,22 @@ solution "carli"
 
   configuration "*"
     if os.get() == "windows" then
-      prebuildcommands { [[cmd /c echo #ifndef GIT_H> git.h~]],
-                         [[cmd /c echo #define GIT_MODIFIED \>> git.h~]],
-                         [[cmd /c git status | grep -c modified >> git.h~]],
-                         [[cmd /c echo #define GIT_REVISION \>> git.h~]],
-                         [[cmd /c git log -n 1 | head -n 1 | sed -e "s/commit //" >> git.h~]],
-                         [[cmd /c echo #define GIT_STR_STR(x) #x>> git.h~]],
-                         [[cmd /c echo #define GIT_STR(x) GIT_STR_STR(x)>> git.h~]],
-                         [[cmd /c echo #define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)>> git.h~]],
-                         [[cmd /c echo #define GIT_REVISION_STR GIT_STR(GIT_REVISION)>> git.h~]],
-                         [[cmd /c echo #endif >>git.h~]],
-                         [[cmd /c diff git.h git.h~ || cp git.h~ git.h]] }
+      prebuildcommands { [[cmd /c echo #ifndef GIT_H>git.h~&&echo #define GIT_MODIFIED \>>git.h~&&git status | grep -c modified>>git.h~&&echo #define GIT_REVISION \>>git.h~&&git log -n 1 | head -n 1 | sed "s/commit //">>git.h~&&echo #define GIT_STR_STR(x) #x>>git.h~&&echo #define GIT_STR(x) GIT_STR_STR(x)>>git.h~&&echo #define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)>>git.h~&&echo #define GIT_REVISION_STR GIT_STR(GIT_REVISION)>>git.h~&&echo #endif>>git.h~&&(diff git.h git.h~||cp git.h~ git.h)]] }
     else
       if _ACTION == "gmake" then
         prebuildcommands { [[$(shell echo "#ifndef GIT_H" > git.h~)]],
                            [[$(shell echo "#define GIT_MODIFIED \\\\" >>git.h~)]],
                            [[$(shell git status | grep -c modified >> git.h~)]],
                            [[$(shell echo "#define GIT_REVISION \\\\" >> git.h~)]],
-                           [[$(shell git log -n 1 | head -n 1 | sed -e 's/commit //' >> git.h~)]],
+                           [[$(shell git log -n 1 | head -n 1 | sed 's/commit //' >> git.h~)]],
                            [[$(shell echo "#define GIT_STR_STR(x) #x" >> git.h~)]],
                            [[$(shell echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h~)]],
                            [[$(shell echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h~)]],
                            [[$(shell echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h~)]],
                            [[$(shell echo "#endif" >> git.h~)]],
-                           [[$(shell diff git.h git.h~ > /dev/null || cp git.h~ git.h; fi)]]}
+                           [[$(shell diff git.h git.h~ > /dev/null || cp git.h~ git.h)]]}
       elseif _ACTION == "codeblocks" then
-        prebuildcommands { [[echo "#ifndef GIT_H" > git.h~]],
-                           [[echo "#define GIT_MODIFIED \\\\" >>git.h~]],
-                           [[git status | grep -c modified >> git.h~]],
-                           [[echo "#define GIT_REVISION \\\\" >> git.h~]],
-                           [[git log -n 1 | head -n 1 | sed -e \'s/commit //\' >> git.h~]],
-                           [[echo "#define GIT_STR_STR(x) #x" >> git.h~]],
-                           [[echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h~]],
-                           [[echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h~]],
-                           [[echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h~]],
-                           [[echo "#endif" >> git.h~]],
-                           [[diff git.h git.h~ > /dev/null || cp git.h~ git.h; fi]]}
+        prebuildcommands { [[echo "#ifndef GIT_H">git.h~&&echo "#define GIT_MODIFIED \\\\">>git.h~&&git status | grep -c modified>>git.h~&&echo "#define GIT_REVISION \\\\">>git.h~&&git log -n 1 | head -n 1 | sed \'s/commit //\'>>git.h~&&echo "#define GIT_STR_STR(x) #x">>git.h~&&echo "#define GIT_STR(x) GIT_STR_STR(x)">>git.h~&&echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)">>git.h~&&echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)">>git.h~&&echo "#endif">>git.h~&&(diff git.h git.h~>/dev/null||cp git.h~ git.h)]]}
       end
     end
 
