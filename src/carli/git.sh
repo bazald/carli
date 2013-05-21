aadcbbbe1338b69dbd38d5da630ea89e0def140c
Mitchell Keith Bloch <bazald@gmail.com>
2013-05-21 03:06:03 -0400

    Fix git.sh again;

--- src/carli/git.sh
+++ src/carli/git.sh
@@ -2,8 +2,11 @@
 
 echo "#ifndef GIT_H" > git.h~ || exit 1
 echo "#define GIT_MODIFIED \\" >> git.h~ || exit 2
-git status | grep -c modified >> git.h~
+
+STATUS=$(git status)
 if [ $? -ne 0 ]; then exit 3; fi
+echo "$STATUS" | grep -c modified >> git.h~
+
 echo "#define GIT_REVISION \\" >> git.h~ || exit 4 
 git log -n 1 | head -n 1 | sed 's/commit //' >> git.h~ || exit 5
 echo "#define GIT_STR_STR(x) #x" >> git.h~ || exit 6
