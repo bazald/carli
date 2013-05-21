9578a1bd8f23cd7604b887d75316c724cd4f46c6
Mitchell Keith Bloch <bazald@gmail.com>
2013-05-21 02:51:59 -0400

    git.sh error codes;

--- src/carli/git.sh
+++ src/carli/git.sh
@@ -1,11 +1,11 @@
-echo "#ifndef GIT_H" > git.h~
-echo "#define GIT_MODIFIED \\\\" >> git.h~
-git status | grep -c modified >> git.h~
-echo "#define GIT_REVISION \\\\" >> git.h~
-git log -n 1 | head -n 1 | sed 's/commit //' >> git.h~
-echo "#define GIT_STR_STR(x) #x" >> git.h~
-echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h~
-echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h~
-echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h~
-echo "#endif" >> git.h~
-(diff git.h git.h~ > /dev/null || cp git.h~ git.h)
+echo "#ifndef GIT_H" > git.h~ || exit 1
+echo "#define GIT_MODIFIED \\\\" >> git.h~ || exit 2
+git status | grep -c modified >> git.h~ || exit 3
+echo "#define GIT_REVISION \\\\" >> git.h~ || exit 4 
+git log -n 1 | head -n 1 | sed 's/commit //' >> git.h~ || exit 5
+echo "#define GIT_STR_STR(x) #x" >> git.h~ || exit 6
+echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h~ || exit 7
+echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h~ || exit 8
+echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h~ || exit 9
+echo "#endif" >> git.h~ || exit 10
+(diff git.h git.h~ > /dev/null || cp git.h~ git.h) || exit 11
