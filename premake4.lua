2804bf2c3f2a56e40f503380c563fa907b08e08c
Mitchell Keith Bloch <bazald@gmail.com>
2013-05-21 00:46:03 -0400

    Make git.h on Linux match git.h on Windows;

--- premake4.lua
+++ premake4.lua
@@ -52,15 +52,25 @@ solution "carli"
     else
       if _ACTION == "gmake" then
         prebuildcommands { [[$(shell echo "#ifndef GIT_H" > git.h)]],
-                           [[$(shell echo "#define GIT_MODIFIED $$(git status | grep -c modified)" >> git.h)]],
-                           [[$(shell echo "#define GIT_MODIFIED_STR \"$$(git status | grep -c modified)\"" >> git.h)]],
-                           [[$(shell echo "#define GIT_REVISION_STR \"$$(git log -n 1 | head -n 1 | sed 's/commit //')\"" >> git.h)]],
+                           [[$(shell echo "#define GIT_MODIFIED \\\\" >>git.h)]],
+                           [[$(shell git status | grep -c modified >> git.h)]],
+                           [[$(shell echo "#define GIT_REVISION \\\\" >> git.h)]],
+                           [[$(shell git log -n 1 | head -n 1 | sed 's/commit //' >> git.h)]],
+                           [[$(shell echo "#define GIT_STR_STR(x) #x" >> git.h)]],
+                           [[$(shell echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h)]],
+                           [[$(shell echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h)]],
+                           [[$(shell echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h)]],
                            [[$(shell echo "#endif" >> git.h)]] }
       elseif _ACTION == "codeblocks" then
         prebuildcommands { [[echo "#ifndef GIT_H" > git.h]],
-                           [[echo "#define GIT_MODIFIED $$(git status | grep -c modified)" >> git.h]],
-                           [[echo "#define GIT_MODIFIED_STR \\\"$$(git status | grep -c modified)\\\"" >> git.h]],
-                           [[echo "#define GIT_REVISION_STR \\\"$$(git log -n 1 | head -n 1 | sed \'s/commit \/\/\')\\\"" >> git.h]],
+                           [[echo "#define GIT_MODIFIED \\\\" >>git.h]],
+                           [[git status | grep -c modified >> git.h]],
+                           [[echo "#define GIT_REVISION \\\\" >> git.h]],
+                           [[git log -n 1 | head -n 1 | sed \'s\/commit \/\/\' >> git.h]],
+                           [[echo "#define GIT_STR_STR(x) #x" >> git.h]],
+                           [[echo "#define GIT_STR(x) GIT_STR_STR(x)" >> git.h]],
+                           [[echo "#define GIT_MODIFIED_STR GIT_STR(GIT_MODIFIED)" >> git.h]],
+                           [[echo "#define GIT_REVISION_STR GIT_STR(GIT_REVISION)" >> git.h]],
                            [[echo "#endif" >> git.h]] }
       end
     end
